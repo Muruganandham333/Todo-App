b047a201a27d8cc3b65ac44c77fc4cb0deb736e2
Muruganandham <muruganandham.elangovan@roundsedge.com>
2023-08-03 05:24:53 +0530

    base design work done

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="App">
+      <div className="todo-container">
+        <h1>TODO LIST</h1>
+        <div className="todo-header">
+          <button type="submit" className="primary-btn">
+            Add Task
+          </button>
+          <select name="filter" id="filter">
+            <option value="all">All</option>
+            <option value="all">Incomplete</option>
+            <option value="all">Completed</option>
+          </select>
+        </div>
+        <div className="todo-list-container">
+          <div className="todo-list">
+            <div className="flex five-gap">
+              <input
+                type="checkbox"
+                className="todo-check-box"
+                name="todo"
+                id="todo"
+              />
+              <div className="todo-info-container">
+                <p className="todo-info">First</p>
+                <p className="todo-info">9:57 PM, 07/31/2023</p>
+              </div>
+            </div>
+            <div className="flex btn-gap">
+              <button type="button">Delete</button>
+              <button type="button">Edit</button>
+            </div>
+          </div>
+
+          <div className="todo-list">
+            <div className="flex five-gap">
+              <input
+                type="checkbox"
+                className="todo-check-box"
+                name="todo"
+                id="todo"
+              />
+              <div className="todo-info-container">
+                <p className="todo-info">Second</p>
+                <p className="todo-info">9:59 PM, 07/31/2023</p>
+              </div>
+            </div>
+            <div className="flex btn-gap">
+              <button type="button">Delete</button>
+              <button type="button">Edit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Modal />
+    </div>
+  );
+  function handleCreateTodo() {
+    console.log("Add Todo Clicked");
+    setOpen(true);
+  }
+
+  function Modal() {
+    return (
+      <div>
+        <Modal open={open}>
+          <h3>Add Todo Form</h3>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default App;
